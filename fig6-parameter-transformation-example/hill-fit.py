9dee0e7058c68a453344bd001439cd8eb67d7056
Chon Lok Lei <chonloklei@gmail.com>
2019-10-26 12:16:47 +0100

    Tidy up

--- fig6-parameter-transformation-example/hill-fit.py
+++ fig6-parameter-transformation-example/hill-fit.py
@@ -23,7 +23,9 @@ def transformed_func(x, tt1, tt2):
     ut2 = np.copy(tt2)
     return hill(x, ut1, ut2)
 
+#
 # Generate
+#
 xdata = np.array([0.1, 0.5, 1.0, 5.0, 10.0])
 xfunc = np.logspace(-5., 5., 200)
 ic50_true = 1.2
@@ -42,7 +44,9 @@ ic50l = 0.01
 nu = 10.
 nl = 0.0
 
+#
 # Simple fit
+#
 p0_all = []
 popt_all = []
 for _ in range(18):
@@ -51,7 +55,7 @@ for _ in range(18):
             bounds=([ic50l, nl], [ic50u, nu]))
     p0_all.append(p0)
     popt_all.append(popt)
-# Manual init
+# Manual init for illustration
 p0 = [1., 0.3]
 popt, pcov = curve_fit(hill, xdata, ydata, p0=p0,
         bounds=([ic50l, nl], [ic50u, nu]))
@@ -62,6 +66,8 @@ popt, pcov = curve_fit(hill, xdata, ydata, p0=p0,
         bounds=([ic50l, nl], [ic50u, nu]))
 p0_all.append(p0)
 popt_all.append(popt)
+
+# Plot illustration
 plt.plot(xdata, ydata, 'x')
 plt.plot(xfunc, hill(xfunc, *popt), c='#ff7f0e')
 plt.text(0.25, 0.15, 'A', fontsize=14, color='#ff7f0e')
@@ -97,8 +103,6 @@ y_min, y_max = np.min(n_sweep), np.max(n_sweep)
 z_min, z_max = 0.05, 0.6
 fig, ax = plt.subplots(figsize=(5.5, 5))
 c = ax.pcolormesh(IC50, N, E, cmap='viridis_r', vmin=z_min, vmax=z_max)
-# cmap: 'RdBu', 'YlGnBu'
-#ax.plot(ic50_true, n_true, marker='x', c='w', ls='')
 for i, (p0, popt) in enumerate(zip(p0_all, popt_all)):
     if i == len(p0_all) - 1:
         colour = '#fdbf6f'
@@ -115,8 +119,6 @@ ax.text(115., 0.8, 'C', fontsize=14, color='#fdbf6f', ha='left', va='top')
 ax.axis([x_min, x_max, y_min, y_max])
 ax.set_xlabel(r'IC$_{50}$', fontsize=17)
 ax.set_ylabel('Hill coefficient', fontsize=17)
-#cbar = fig.colorbar(c, ax=ax)
-#cbar.ax.set_ylabel('RMSE', fontsize=17)
 plt.subplots_adjust(wspace=0, hspace=0)
 plt.tight_layout()
 plt.savefig('hill-fig/dose-response-simple-fit', dpi=200, bbox_inches='tight')
@@ -124,14 +126,28 @@ plt.savefig('hill-fig/dose-response-simple-fit.pdf', format='pdf',
         bbox_inches='tight')
 plt.close()
 
-'''
+#
 # Simple transformed fit
+#
+tp0_all = []
 tpopt_all = []
-for p0 in p0_all:
-    tp0 = [np.log(p0[0]), p0[1]]
+for _ in range(18):
+    tp0 = np.random.uniform([np.log(ic50l), nl], [np.log(ic50u), nu])
     tpopt, pcov = curve_fit(transformed_func, xdata, ydata, p0=tp0,
             bounds=([np.log(ic50l), nl], [np.log(ic50u), nu]))
+    tp0_all.append(tp0)
     tpopt_all.append(tpopt)
+# Manual init for illustration
+tp0 = [np.log(1.), 0.3]
+tpopt, pcov = curve_fit(transformed_func, xdata, ydata, p0=tp0,
+        bounds=([np.log(ic50l), nl], [np.log(ic50u), nu]))
+tp0_all.append(tp0)
+tpopt_all.append(tpopt)
+tp0 = [np.log(100.), 1.]
+tpopt, pcov = curve_fit(transformed_func, xdata, ydata, p0=tp0,
+        bounds=([np.log(ic50l), nl], [np.log(ic50u), nu]))
+tp0_all.append(tp0)
+tpopt_all.append(tpopt)
 
 # Inspect transformed contour
 tic50_sweep = np.log(np.logspace(np.log10(ic50l), np.log10(ic50u), 250))
@@ -144,71 +160,13 @@ for i in range(TIC50.shape[0]):
         TE[i, j] = error(np.exp(TIC50[i, j]), TN[i, j])
 x_min, x_max = np.min(tic50_sweep), np.max(tic50_sweep)
 y_min, y_max = np.min(tn_sweep), np.max(tn_sweep)
-z_min, z_max = np.min(TE), np.max(TE)
-fig, ax = plt.subplots()
-c = ax.pcolormesh(TIC50, TN, TE, cmap='viridis_r', vmin=z_min, vmax=z_max)
-# cmap: 'RdBu', 'YlGnBu'
-#ax.plot(np.log(ic50_true), n_true, marker='x', c='w', ls='')
-for p0, tpopt in zip(p0_all, tpopt_all):
-    ax.plot([np.log(p0[0]), tpopt[0]], [p0[1], tpopt[1]], marker='x',
-            c='#cccccc', ls='--', alpha=1)
-    ax.plot(tpopt[0], tpopt[1], marker='x', c='#ff7f0e', ls='')
-ax.axis([x_min, x_max, y_min, y_max])
-ax.set_xlabel(r'$\ln$(IC$_{50})$')
-ax.set_ylabel('Hill coefficient')
-cbar = fig.colorbar(c, ax=ax)
-cbar.ax.set_ylabel('RMSE')
-plt.subplots_adjust(wspace=0, hspace=0)
-plt.tight_layout()
-plt.close()
-'''
-
-# Simple transformed fit (resampled p0)
-#def transformed2_func(x, tt1, tt2):
-#    ut1 = np.exp(tt1)
-#    ut2 = np.exp(tt2)
-#    return hill(x, ut1, ut2)
-t2p0_all = []
-t2popt_all = []
-for _ in range(18):
-    t2p0 = np.random.uniform([np.log(ic50l), nl], [np.log(ic50u), nu])
-    t2popt, pcov = curve_fit(transformed_func, xdata, ydata, p0=t2p0,
-            bounds=([np.log(ic50l), nl], [np.log(ic50u), nu]))
-    t2p0_all.append(t2p0)
-    t2popt_all.append(t2popt)
-# Manual init
-t2p0 = [np.log(1.), 0.3]
-t2popt, pcov = curve_fit(transformed_func, xdata, ydata, p0=t2p0,
-        bounds=([np.log(ic50l), nl], [np.log(ic50u), nu]))
-t2p0_all.append(t2p0)
-t2popt_all.append(t2popt)
-t2p0 = [np.log(100.), 1.]
-t2popt, pcov = curve_fit(transformed_func, xdata, ydata, p0=t2p0,
-        bounds=([np.log(ic50l), nl], [np.log(ic50u), nu]))
-t2p0_all.append(t2p0)
-t2popt_all.append(t2popt)
-
-# Inspect transformed contour (resampled p0)
-t2ic50_sweep = np.log(np.logspace(np.log10(ic50l), np.log10(ic50u), 250))
-#t2n_sweep = np.log(np.logspace(np.log10(nl), np.log10(nu), 250))
-t2n_sweep = np.copy(n_sweep)
-error = Error(ydata, xdata)
-T2IC50, T2N = np.meshgrid(t2ic50_sweep, t2n_sweep)
-T2E = np.zeros(T2IC50.shape)
-for i in range(T2IC50.shape[0]):
-    for j in range(T2IC50.shape[1]):
-        T2E[i, j] = error(np.exp(T2IC50[i, j]), T2N[i, j])
-x_min, x_max = np.min(t2ic50_sweep), np.max(t2ic50_sweep)
-y_min, y_max = np.min(t2n_sweep), np.max(t2n_sweep)
 z_min, z_max = 0.05, 0.6
 fig, ax = plt.subplots(figsize=(6, 5))
-c = ax.pcolormesh(T2IC50, T2N, T2E, cmap='viridis_r', vmin=z_min, vmax=z_max)
-# cmap: 'RdBu', 'YlGnBu'
-#ax.plot(np.log(ic50_true), n_true, marker='x', c='w', ls='')
-for i, (tp0, tpopt) in enumerate(zip(t2p0_all, t2popt_all)):
-    if i == len(t2p0_all) - 1:
+c = ax.pcolormesh(TIC50, TN, TE, cmap='viridis_r', vmin=z_min, vmax=z_max)
+for i, (tp0, tpopt) in enumerate(zip(tp0_all, tpopt_all)):
+    if i == len(tp0_all) - 1:
         colour = '#fdbf6f'
-    elif i == len(t2p0_all) - 2:
+    elif i == len(tp0_all) - 2:
         colour = '#e41a1c'
     else:
         colour = '#cccccc'
